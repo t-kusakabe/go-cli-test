de0682807c84c82d3c5dc644abe17bb9de448c6a
t-kusakabe <ttmac0704@gmail.com>
2018-03-07 00:32:59 +0900

    Add boolean option

--- main.go
+++ main.go
@@ -12,39 +12,25 @@ func main() {
 	app.Usage = "Sample is foo!"
 	app.Version = "1.0"
 
-	os.Setenv("SAMPLE_ENV", "sample env")
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "lang, l",
-			Value: "english",
-			Usage: "language for the greeting",
+			Name:  "name, n",
+			Value: "tarou",
+			Usage: "your name",
 		},
-		cli.StringFlag{
-			Name:  "meridian, m",
-			Value: "AM",
-			Usage: "meridian for the greeting",
-		},
-		cli.StringFlag{
-			Name:  "time, t",
-			Value: "07:00",
-			Usage: "'your time' for the greeting",
-		},
-		cli.StringFlag{
-			Name:   "aaa, a",
-			Value:  "sample",
-			EnvVar: "SAMPLE_ENV",
-		},
+    cli.BoolFlag {
+      Name: "gay, g",
+      Usage: "are you gay boy?",
+    },
 	}
 
 	app.Action = func(c *cli.Context) error {
 		fmt.Println("-- Action ==")
 
-		fmt.Printf("c.GlobalFlagNames()         :%+v\n", c.GlobalFlagNames())
-		fmt.Printf("c.String(\"lang\")          :%+v\n", c.String("lang"))
-		fmt.Printf("c.String(\"m\")             :%+v\n", c.String("m"))
-		fmt.Printf("c.String(\"time\")          :%+v\n", c.String("time"))
-		fmt.Printf("c.String(\"a\")             :%+v\n", c.String("a"))
+		fmt.Printf("c.GlobalFlagNames()       :%+v\n", c.GlobalFlagNames())
+		fmt.Printf("c.String(\"name\")        :%+v\n", c.String("name"))
+		fmt.Printf("c.Bool(\"g\")             :%+v\n", c.Bool("g"))
 
 		cli.ShowVersion(c)
 
