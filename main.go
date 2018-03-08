767dd8cfd037daf0ae0a3d7cfc3913bca0fe7145
t-kusakabe <ttmac0704@gmail.com>
2018-03-08 14:01:35 +0900

    Use sub command

--- main.go
+++ main.go
@@ -12,29 +12,48 @@ func main() {
 	app.Usage = "Sample is foo!"
 	app.Version = "1.0"
 
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "name, n",
-			Value: "tarou",
-			Usage: "your name",
+	app.Commands = []cli.Command{
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
 		},
-    cli.BoolFlag {
-      Name: "gay, g",
-      Usage: "are you gay boy?",
-    },
-	}
-
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("-- Action ==")
-
-		fmt.Printf("c.GlobalFlagNames()       :%+v\n", c.GlobalFlagNames())
-		fmt.Printf("c.String(\"name\")        :%+v\n", c.String("name"))
-		fmt.Printf("c.Bool(\"g\")             :%+v\n", c.Bool("g"))
-
-		cli.ShowVersion(c)
-
-		return nil
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("completed task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "template",
+			Aliases: []string{"t"},
+			Usage:   "options for task templates",
+			Subcommands: []cli.Command{
+				{
+					Name:  "add",
+					Usage: "add a new template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("new task template: ", c.Args().First())
+						return nil
+					},
+				},
+				{
+					Name:  "remove",
+					Usage: "remove an existing template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("remove task tempalte: ", c.Args().First())
+						return nil
+					},
+				},
+			},
+		},
 	}
 
 	app.Run(os.Args)
